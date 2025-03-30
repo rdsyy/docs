abbccf53f810ae55610195f234eccfb9addfd00c
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-03-30 19:20:30 -0400

    remove built with

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} RANDAO. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} RANDAO.`,
     },
     prism: {
       theme: prismThemes.github,
